541c9b8bd297e7837b556b078a64a248ba9cb308
Øyvind Johannessen <oyvind.bremar.johannessen@dnb.no>
2024-12-06 13:06:08 +0100

    testing goroutines

--- day06/main.go
+++ day06/main.go
@@ -2,21 +2,72 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/oyjoh/advent-of-code-2024/util"
 )
 
 func main() {
-	example := "in_ex.txt"
+	example := "in_1.txt"
 	matrix := util.RuneMatrix(example)
 	matrix = util.PadRuneMatrix(matrix, 1)
 
 	//fmt.Println(pt1(matrix))
+	start := time.Now()
+	fmt.Println(pt2Concurrent(matrix))
+	elapsed := time.Since(start)
+	fmt.Printf("concurrent version took %s\n", elapsed)
+
+	matrix = util.RuneMatrix(example)
+	matrix = util.PadRuneMatrix(matrix, 1)
+
+	//fmt.Println(pt1(matrix))
+	start = time.Now()
 	fmt.Println(pt2(matrix))
+	elapsed = time.Since(start)
+	fmt.Printf("dumb version took %s\n", elapsed)
+}
+
+func pt2Concurrent(matrix [][]rune) int {
+	cntObstacles := 0
+
+	var wg sync.WaitGroup
+	results := make(chan int, len(matrix)*len(matrix[0]))
+
+	for y := 1; y < len(matrix)-1; y++ {
+		for x := 1; x < len(matrix[0])-1; x++ {
+			if matrix[y][x] == '.' {
+				wg.Add(1)
+				go func(y, x int) {
+					defer wg.Done()
+					matrixCopy := make([][]rune, len(matrix))
+					for i := range matrix {
+						matrixCopy[i] = make([]rune, len(matrix[i]))
+						copy(matrixCopy[i], matrix[i])
+					}
+					matrixCopy[y][x] = '#'
+					if pt1(matrixCopy) == -1 {
+						results <- 1
+					} else {
+						results <- 0
+					}
+				}(y, x)
+			}
+		}
+	}
+
+	wg.Wait()
+	close(results)
+
+	for result := range results {
+		cntObstacles += result
+	}
+
+	return cntObstacles
 }
 
 func pt2(matrix [][]rune) int {
-	util.PrintRuneMatrix(matrix)
 	cntObstacles := 0
 
 	for y := 1; y < len(matrix)-1; y++ {
