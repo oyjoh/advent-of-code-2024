53aa0dad3da6c00e9626e67fbb7302f9493c226f
Øyvind Bremar Johannessen <oyvind-miles@yvind-sin-MacBook-Pro.local>
2024-12-14 21:26:24 +0100

    day13

--- day13/main.go
+++ day13/main.go
@@ -2,15 +2,62 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
-	matrix := readInp("in_ex.txt")
-	fmt.Println(matrix)
-	fmt.Printf("Part 1: %d", pt1(matrix))
+	matrix := readInp("in_1.txt")
+	//fmt.Println(matrix)
+	fmt.Printf("Part 1: %d\n", pt1(matrix))
+	fmt.Printf("Part 2: %d\n", pt2(matrix))
+}
+
+func pt2(matrix [][]int) int64 {
+	var cost int64 = 0
+
+	for _, machine := range matrix {
+
+		ay, ax, by, bx, py, px := machine[0], machine[1], machine[2], machine[3], machine[4], machine[5]
+
+		// part 2
+		var fpy int64 = int64(py) + 10000000000000
+		var fpx int64 = int64(px) + 10000000000000
+
+		A := mat.NewDense(2, 2, []float64{float64(ay), float64(by), float64(ax), float64(bx)})
+		B := mat.NewVecDense(2, []float64{float64(fpy), float64(fpx)})
+
+		var x mat.VecDense
+		if err := x.SolveVec(A, B); err != nil {
+			fmt.Println(err)
+		}
+		a := x.RawVector().Data[0]
+		b := x.RawVector().Data[1]
+
+		// check if we can convert to int
+		const epsilon = 1e-5 // Margin of error
+		if _, frac := math.Modf(math.Abs(a)); frac < epsilon || frac > 1.0-epsilon {
+			if _, frac := math.Modf(math.Abs(b)); frac < epsilon || frac > 1.0-epsilon {
+				aI := int64(math.Round(a))
+				bI := int64(math.Round(b))
+
+				if aI >= 0 && bI >= 0 {
+					//fmt.Println(machine)
+					// fmt.Printf("a: %d, b: %d\n", aI, bI)
+					// fmt.Println(x.RawVector().Data)
+					cost += aI * 3
+					cost += bI
+				}
+			}
+		}
+
+	}
+
+	return cost
 }
 
 func pt1(matrix [][]int) int {
@@ -18,36 +65,38 @@ func pt1(matrix [][]int) int {
 
 	for _, machine := range matrix {
 
-		cost += press(0, 0, 0, 0, 0, machine)
+		ay, ax, by, bx, py, px := machine[0], machine[1], machine[2], machine[3], machine[4], machine[5]
 
-		break
+		A := mat.NewDense(2, 2, []float64{float64(ay), float64(by), float64(ax), float64(bx)})
+		B := mat.NewVecDense(2, []float64{float64(py), float64(px)})
+
+		var x mat.VecDense
+		if err := x.SolveVec(A, B); err != nil {
+			fmt.Println(err)
+		}
+		a := x.RawVector().Data[0]
+		b := x.RawVector().Data[1]
+
+		// check if we can convert to int
+		const epsilon = 1e-9 // Margin of error
+		if _, frac := math.Modf(math.Abs(a)); frac < epsilon || frac > 1.0-epsilon {
+			if _, frac := math.Modf(math.Abs(b)); frac < epsilon || frac > 1.0-epsilon {
+				aI := int(math.Round(a))
+				bI := int(math.Round(b))
+
+				if aI <= 100 && bI <= 100 && aI >= 0 && bI >= 0 {
+					// fmt.Println(machine)
+					// fmt.Printf("a: %d, b: %d\n", aI, bI)
+					// fmt.Println(x.RawVector().Data)
+					cost += aI * 3
+					cost += bI
+				}
+			}
+		}
+
 	}
 
 	return cost
-}
-
-func press(cost, y, x, pa, pb int, dat []int) int {
-	fmt.Printf("y: %d, x: %d\n", y, x)
-	fmt.Printf("pa: %d, pb: %d\n", pa, pb)
-
-	if y == dat[4] && x == dat[5] {
-		return cost
-	}
-	if y > dat[4] || x > dat[5] {
-		return 0
-	}
-	if pa >= 100 || pb >= 100 {
-		return 0
-	}
-
-	a, b := press(cost+3, y+dat[0], x+dat[1], pa+1, pb, dat), press(cost+1, y+dat[2], x+dat[3], pa, pb+1, dat)
-
-	if a > b && b > 0 {
-		return b
-	} else {
-		return a
-	}
-
 }
 
 func readInp(path string) [][]int {
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/oyjoh/advent-of-code-2024
 go 1.23.2
 
 require github.com/deckarep/golang-set/v2 v2.7.0
+
+require gonum.org/v1/gonum v0.15.1
